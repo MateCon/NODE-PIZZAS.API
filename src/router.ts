943f29ff2672a9db86fbe265a254f9dfb472586b
MateCon <mateoziffer@gmail.com>
2022-04-13 10:32:31 -0300

    .

--- src/router.ts
+++ src/router.ts
@@ -2,18 +2,20 @@ import { Application } from "express";
 import fs from "fs";
 import path from "path";
 
-const router = (app: Application) => {
-	fs.readdir(path.join(__dirname, "controllers"), (err, dir) => {
-		const controllers = dir.filter((file) => /Controller/.test(file));
-		controllers.forEach((controller) => {
-			const route = require(path.join(
-				__dirname,
-				"controllers",
-				controller
-			)).default;
-			app.use(`/${controller.split("Controller")[0]}`, route);
+const router = async (app: Application) =>
+	new Promise<Application>((resolve, reject) => {
+		fs.readdir(path.join(__dirname, "controllers"), (err, dir) => {
+			const controllers = dir.filter((file) => /Controller/.test(file));
+			controllers.forEach((controller) => {
+				const route = require(path.join(
+					__dirname,
+					"controllers",
+					controller
+				)).default;
+				app.use(`/${controller.split("Controller")[0]}`, route);
+				resolve(app);
+			});
 		});
 	});
-};
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import express from "express";
+import express, { Application } from "express";
 import cors from "cors";
 import router from "./router";
 
-const app = express();
-const port = 8080;
+(async () => {
+	const app: Application = await router(express());
+	const port = 8080;
 
-app.use(cors());
-app.use(express.json());
+	app.use(cors());
+	app.use(express.json());
 
-router(app);
+	app.get("*", (_, res) => {
+		res.status(404).send("404 error - page not found");
+	});
 
-app.get("*", (_, res) => {
-	res.status(404).send("404 error - page not found");
-});
-
-app.listen(port, () => {
-	console.log(`Server running on port ${port}`);
-});
+	app.listen(port, () => {
+		console.log(`Server running on port ${port}`);
+	});
+})();
