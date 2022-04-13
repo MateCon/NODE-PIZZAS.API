8a45691c707c25bf559fab0b4f3df7476383b6dc
Mateo Ziffer <68882595+MateCon@users.noreply.github.com>
2022-04-13 19:02:20 -0300

    Delete build directory

--- build/controllers/pizzaController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var route = (0, express_1.Router)();
-route.get("/", function (_, res) {
-    res.send("hello");
-});
-exports.default = route;
