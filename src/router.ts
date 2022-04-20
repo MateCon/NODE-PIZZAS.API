a53c06e81a0771b0efd28cb171bf42c53a80d3c3
MateCon <mateoziffer@gmail.com>
2022-04-20 08:09:24 -0300

    .

--- src/router.ts
+++ src/router.ts
@@ -2,17 +2,15 @@ import { Application } from "express";
 import { readdirSync } from "fs";
 import { join } from "path";
 
-const router = (app: Application) =>
-	new Promise<Application>((resolve) => {
-		const dir = readdirSync(join(__dirname, "controllers"));
-		dir.forEach((filename) => {
-			if (!/Controller/.test(filename)) return;
-			app.use(
-				`/${filename.split("Controller")[0]}`,
-				require(join(__dirname, "controllers", filename)).default
-			);
-		});
-		resolve(app);
+const router = (app: Application): Application => {
+	readdirSync(join(__dirname, "controllers")).forEach((filename) => {
+		if (!/Controller/.test(filename)) return;
+		app.use(
+			`/${filename.split("Controller")[0]}`,
+			require(join(__dirname, "controllers", filename)).default
+		);
 	});
+	return app;
+};
 
 export default router;
